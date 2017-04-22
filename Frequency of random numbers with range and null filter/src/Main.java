892de720eddf3a5e6ea10ad5dec49365b82ac56c
Blade Wolfmoon <2CPT1MP@gmail.com>
2017-04-22 18:17:33 +0300

    Frequency of random numbers with range and null filter
    Frequency of random numbers with range and null filter

---
+++ Frequency of random numbers with range and null filter/src/Main.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+import java.util.Random;
+public class Main{
+    public static void main(String args[]){
+
+        Scanner scannerObject1=new Scanner(System.in);  //GETTING USER INPUT
+        System.out.println("Enter min number:");
+        int input1=scannerObject1.nextInt();
+
+        Scanner scannerObject2=new Scanner(System.in);    //GETTING USER INPUT
+        System.out.println("Enter max number:");
+        int input2=scannerObject1.nextInt();
+
+        Scanner scannerObject3=new Scanner(System.in);
+        System.out.println("Enter amount of numbers:");   //GETTING USER INPUT
+        int input3=scannerObject1.nextInt();
+
+        Random randomObject=new Random();
+        int storage[]=new int[input1+input3+input2];
+
+        for(int counter=1; counter<=input3; counter++){
+            storage[input1+randomObject.nextInt(input2)]+=1;
+
+        }
+
+        System.out.println("NUMBER \t FREQUENCY");
+
+        for(int counter=1; counter<storage.length; counter++){
+            if(storage[counter]!=0){
+                System.out.println(counter +"\t" +storage[counter]);
+            }
+
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
